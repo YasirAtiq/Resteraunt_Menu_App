47452a20168e6971ce837fc1d64cf14457f21855
Yasir <DPSA00609@mypaceschool.com>
2023-11-02 19:00:47 +0400

    Creating Admin Interface to Add Items.

--- restaurant_app/admin.py
+++ restaurant_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Meal
 
-# Register your models here.
+
+class MealItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status", )
+    search_fields = ("meal", "ingredients")
+
+admin.site.register(Meal, MealItemAdmin)
