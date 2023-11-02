b2c360dc154906d5beb66c9f5fb4147da92318c4
Yasir <DPSA00609@mypaceschool.com>
2023-11-02 18:48:12 +0400

    Implementing Context.

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Meal
@@ -6,6 +7,10 @@ from .models import Meal
 class Menu(generic.ListView):
     queryset = Meal.objects.order_by("meal")
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = {"meals": "Pizza"}
+        return context
 
 
 class MenuItem(generic.DetailView):
