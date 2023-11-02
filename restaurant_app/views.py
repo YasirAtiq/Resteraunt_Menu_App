2aeba9934c9bfef351b6335ae556973190a2a878
Yasir <DPSA00609@mypaceschool.com>
2023-11-02 18:42:18 +0400

    Adding Frontend of our Website.

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Meal
 
-# Create your views here.
+
+class Menu(generic.ListView):
+    queryset = Meal.objects.order_by("meal")
+    template_name = "index.html"
+
+
+class MenuItem(generic.DetailView):
+    model = Meal
+    template_name = "meal_view_detail.html"
